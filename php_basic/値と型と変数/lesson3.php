17c2e9937a2e864eb255aa550f6542991908293d
WATABE YURIKA <yurika@WATABEnoMacBook-Pro.local>
2024-02-08 16:44:32 +0900

    php値と型と変数の課題提出

---
+++ php_basic/値と型と変数/lesson3.php
@@ -0,0 +1,18 @@
+﻿<?php
+$user = [
+    "name" => "山田",
+    "age" => 20,
+    "gender" => "女性",
+];
+echo $user["name"];
+echo "<br/>";
+echo $user["age"],"歳・",$user["gender"];
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
